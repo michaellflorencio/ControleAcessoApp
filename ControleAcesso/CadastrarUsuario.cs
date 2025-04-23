464dc4f1455d37fe89b92204774d9eda2fae42ee
MICHAEL DE LIMA FLORENCIO <michael.lflorencio@senacsp.edu.br>
2025-04-23 11:13:42 -0300

    Adicionar arquivos de projeto.

---
+++ ControleAcesso/CadastrarUsuario.cs
@@ -0,0 +1,10 @@
+namespace ControleAcesso
+{
+    public partial class CadastrarUsuario : Form
+    {
+        public CadastrarUsuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ControleAcesso/FrmConsultaAcesso.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleAcesso
+{
+    public partial class FrmConsultaAcesso: Form
+    {
+        public FrmConsultaAcesso()
+        {
+            InitializeComponent();
+        }
+    }
+}
